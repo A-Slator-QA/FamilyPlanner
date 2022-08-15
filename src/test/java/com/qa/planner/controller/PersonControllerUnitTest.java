7c5e240598f100f920d7aa1e42fe2f78d97c7336
A-Slator-QA <andrew.slator95@gmail.com>
2022-08-15 15:05:49 +0100

    added first service & controller unit tests

--- src/test/java/com/qa/planner/controller/PersonControllerUnitTest.java
+++ src/test/java/com/qa/planner/controller/PersonControllerUnitTest.java
@@ -1,9 +1,15 @@
 package com.qa.planner.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.List;
+
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +44,21 @@ public class PersonControllerUnitTest {
 		mvc.perform(post("/home/createPerson").content(testPersonAsJSON).contentType(MediaType.APPLICATION_JSON))
 				.andExpect(status().isCreated()).andExpect(content().json(testPersonAsJSON));
 		Mockito.verify(this.service, Mockito.times(1)).createPerson(testPerson);
+	}
+
+	@Test
+	public void readPerson_ValidPerson_ReadAll() throws Exception {
+		Person readPerson = new Person();
+		readPerson.setName("Robert");
+		List<Person> allPeople = Arrays.asList(readPerson);
+
+		// given(this.service .readAllPeople()).willReturn(allPeople);
+
+		Mockito.when(this.service.readAllPeople().thenReturn(allPeople));
+
+		mvc.perform(get("/home/readPeople").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(jsonPath("$", hasSize(1))).andExpect(jsonPath("$[0].name", is(readPerson.getName())));
+		Mockito.veridy(this.service, Mockito.times(1)).readAllPeople();
 	}
 
 }
--- src/test/java/com/qa/planner/service/PersonServiceUnitTest.java
+++ src/test/java/com/qa/planner/service/PersonServiceUnitTest.java
@@ -2,6 +2,8 @@ package com.qa.planner.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,6 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.qa.planner.model.Person;
 import com.qa.planner.repo.PersonRepo;
-import com.qa.planner.service.PersonService;
 
 @SpringBootTest
 public class PersonServiceUnitTest {
@@ -25,13 +26,37 @@ public class PersonServiceUnitTest {
 
 	@Test
 	public void createPerson_ValidPerson_Person() {
-		Person savePerson = new Person("Anthony", 25, false);
+		Person savePerson = new Person("Anthony", 26, false);
 		Person repoPerson = new Person(1L, "Anthony", 26, false);
 
 		Mockito.when(this.service.createPerson(savePerson)).thenReturn(repoPerson);
 		assertEquals(repoPerson, this.repo.save(savePerson));
 
 		Mockito.verify(this.repo, Mockito.times(1)).save(savePerson);
+	}
+
+	@Test
+	public void readPerson_ReadAll() {
+		List<Person> people = new ArrayList();
+		people.add(new Person());
+
+		Mockito.when(this.service.readAllPeople(people)).thenReturn(people);
+		assertEquals(people, this.repo.findAllPeople());
+
+		Mockito.verify(this.repo, Mockito.times(1)).findAll();
+	}
+
+	@Test
+	public void readPerson_ValidId_ReadOne() {
+		Long testId = 1L;
+		Person readPerson = new Person("James", 42, true);
+		Optional<Person> mockOutputValid = Optional.ofNullable(new Person(1L, "James", 42, true));
+		Person expectedOutput = new Person(1L, "James", 42, true);
+
+		Mockito.when(this.repo.findById(testId)).thenReturn(mockOutputValid);
+		assertEquals(expectedOutput, this.service.readPerson(expectedOutput));
+
+		Mockito.verify(this.repo, Mockito.times(1)).findById(testId);
 	}
 
 	@Test
@@ -44,9 +69,17 @@ public class PersonServiceUnitTest {
 		Mockito.when(this.repo.findById(testId)).thenReturn(mockOutputValid);
 		Mockito.when(this.repo.save(expectedOutput)).thenReturn(expectedOutput);
 
-		assertEquals(expectedOutput, this.service.updateTask(expectedOutput, 1L));
+		assertEquals(expectedOutput, this.service.updatePerson(expectedOutput, 1L));
 		Mockito.verify(this.repo, Mockito.times(1)).save(expectedOutput);
 		Mockito.verify(this.repo, Mockito.times(1)).findById(testId);
+	}
+
+	@Test
+	public void deletePerson_ValidId_DeletePerson() {
+		Long testId = 1L;
+		Person deletePerson = new Person("Dan", 25, false);
+		Optional<Person> mockOutputValid = Optional.ofNullable(new Person(1L, "Dan", 25, false));
+
 	}
 
 }
